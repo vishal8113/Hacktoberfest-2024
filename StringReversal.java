91a3a61cf4d01c83627b73043600acfcc085dce7
abcd <ujjvalraghavendral@gmail.com>
2024-10-21 23:56:54 +0530

    Added a new problem/solution for XYZ problem

---
+++ StringReversal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class StringReversal {
+    public static String reverseString(String s) {
+        char[] charArray = s.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+        String reversed = reverseString(input);
+        System.out.println("Reversed String: " + reversed);
+
+        scanner.close();
+    }
+}
