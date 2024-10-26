9071c907a026a582329f264c46d85132cefce3ec
jobs4all <bansalsehaj528@gmail.com>
2024-10-26 12:28:00 +0530

    Added selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,4 @@
-public class selsort {
+public class SelectionSort {
     public static void printarr(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -24,4 +24,5 @@ public class selsort {
         printarr(arr);
     }
 }
+
 
